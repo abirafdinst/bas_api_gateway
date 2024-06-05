d6c99ed82d1bf5c65445ca48abcd6bc0c1b9440f
abirafdinst <abirafdi13101@gmail.com>
2024-06-05 14:21:55 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	auth "api_gateway/usecase"
+	"fmt"
+)
+
+func main() {
+
+	login := auth.NewLogin()
+
+	username := "admin"
+	password := "admin123"
+
+	isAuthenticated := login.Authentication(username, password)
+
+	if isAuthenticated {
+		fmt.Println("Login successful")
+	} else {
+		fmt.Println("Invalid username or password")
+	}
+}
